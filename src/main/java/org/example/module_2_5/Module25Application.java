c65c695bbc21fc5d1839bf23b04b05123df295cc
Dinar763 <gizzyatovdinar@gmail.com>
2025-10-13 21:30:10 +0400

    Commit#1

---
+++ src/main/java/org/example/module_2_5/Module25Application.java
@@ -0,0 +1,14 @@
+package org.example.module_2_5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Module25Application {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(Module25Application.class, args);
+    }
+
+}
